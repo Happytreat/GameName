1292885edd128d9166eeb40397d6dff756b4a0b7
Robin Yu <robinccyu@gmail.com>
2019-09-24 05:20:23 +0000

    Add questions API

--- customers/views.py
+++ customers/views.py
@@ -3,14 +3,51 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Customer, Question
+from .models import Customer, Question, QuestionSet
 from .serializers import *
+
+
+@api_view(['GET', 'POST'])
+def question_set(request):
+    """
+    GET: List all question sets along with their corresponding questions.
+    POST: Create a new set of questions.
+    """
+    if request.method == 'GET':
+        data = []
+        nextPage = 1
+        previousPage = 1
+        sets = QuestionSet.objects.all()
+        page = request.GET.get('page', 1)
+        paginator = Paginator(sets, 10)
+        try:
+            data = paginator.page(page)
+        except PageNotAnInteger:
+            data = paginator.page(1)
+        except EmptyPage:
+            data = paginator.page(paginator.num_pages)
+
+        serializer = QuestionSetSerializer(data, context={'request': request}, many=True)
+        if data.has_next():
+            nextPage = data.next_page_number()
+        if data.has_previous():
+            previousPage = data.previous_page_number()
+
+        return Response({'data': serializer.data, 'count': paginator.count, 'numpages': paginator.num_pages,
+                         'nextlink': '/api/sets/?page=' + str(nextPage),
+                         'prevlink': '/api/sets/?page=' + str(previousPage)})
+    if request.method == 'POST':
+        serializer = QuestionSetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'POST'])
 def questions_list(request):
     """
-    List questions
+    List questions, or create a new one.
     """
     if request.method == 'GET':
         data = []
@@ -32,9 +69,51 @@ def questions_list(request):
         if data.has_previous():
             previousPage = data.previous_page_number()
 
-        return Response({'data': serializer.data , 'count': paginator.count, 'numpages' : paginator.num_pages,
+        return Response({'data': serializer.data, 'count': paginator.count, 'numpages': paginator.num_pages,
                          'nextlink': '/api/questions/?page=' + str(nextPage),
                          'prevlink': '/api/questions/?page=' + str(previousPage)})
+    elif request.method == 'POST':
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'POST'])
+def question_sets_list(request):
+    """
+    List question sets, or create a new one.
+    """
+    if request.method == 'GET':
+        data = []
+        nextPage = 1
+        previousPage = 1
+        question_sets = QuestionSet.objects.all()
+        page = request.GET.get('page', 1)
+        paginator = Paginator(question_sets, 10)
+        try:
+            data = paginator.page(page)
+        except PageNotAnInteger:
+            data = paginator.page(1)
+        except EmptyPage:
+            data = paginator.page(paginator.num_pages)
+
+        serializer = QuestionSetSerializer(data, context={'request': request}, many=True)
+        if data.has_next():
+            nextPage = data.next_page_number()
+        if data.has_previous():
+            previousPage = data.previous_page_number()
+
+        return Response({'data': serializer.data, 'count': paginator.count, 'numpages': paginator.num_pages,
+                         'nextlink': '/api/sets/?page=' + str(nextPage),
+                         'prevlink': '/api/sets/?page=' + str(previousPage)})
+    elif request.method == 'POST':
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'POST'])
@@ -70,6 +149,7 @@ def customers_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def customers_detail(request, pk):
