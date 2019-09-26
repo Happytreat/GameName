aa94e57ce1a1363db220c38fce0fc4456f3a995b
Happytreat <cowfats@gmail.com>
2019-09-26 12:26:01 +0800

    Remove django-herku

--- djangoreactproject/settings.py
+++ djangoreactproject/settings.py
@@ -138,7 +138,3 @@ STATICFILES_DIR = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-# Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
